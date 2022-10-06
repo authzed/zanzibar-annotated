39a88068d08d8f2f57dbf26ea93497e62ab63821
Sam Kim <samkim@gmail.com>
2022-10-05 22:53:48 -0700

    Fix tuple notation, tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './content/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     fontFamily: {
