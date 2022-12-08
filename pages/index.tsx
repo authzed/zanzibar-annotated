c6168ca04e45dce368ad948cc178b870d9eb3f89
Sam Kim <samkim@gmail.com>
2022-12-08 09:11:26 -0800

    Add baseUrl for index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,25 @@ import { GetServerSideProps } from 'next';
 import getConfig from 'next/config';
 import Zanzibar from '../content/zanzibar.mdx';
 
-export default function Default(props: { canonicalUrl: string }) {
+export default function Default(props: {
+  canonicalUrl: string;
+  baseUrl: string;
+}) {
   return (
     <>
-      <Zanzibar canonicalUrl={props.canonicalUrl} />
+      <Zanzibar canonicalUrl={props.canonicalUrl} baseUrl={props.baseUrl} />
     </>
   );
 }
 
 export const getServerSideProps: GetServerSideProps<{
   canonicalUrl: string;
+  baseUrl: string;
 }> = async ({ req }) => {
   const { publicRuntimeConfig } = getConfig();
   return {
     props: {
+      baseUrl: publicRuntimeConfig.CanonicalUrlBase,
       canonicalUrl: publicRuntimeConfig.CanonicalUrlBase,
     },
   };
